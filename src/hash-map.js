9809709e6ef18e200efc1a354209fe16f039055e
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-04-10 07:19:04 -0700

    initialize setup and hash map function

---
+++ src/hash-map.js
@@ -0,0 +1,11 @@
+function HashMap() {
+  function hash(key) {
+    let hashCode = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
