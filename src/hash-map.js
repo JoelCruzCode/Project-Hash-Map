0e76400fbb63a49b74ac0b0074240224cdb32c84
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-04-11 16:15:51 -0700

    create functions for inserting values and resizing buckets

--- src/hash-map.js
+++ src/hash-map.js
@@ -1,11 +1,46 @@
 function HashMap() {
+  const buckets = new Array(16).fill(null);
+  const loadThreshold = 0.75;
+
+  function getLoadFactor(buckets) {
+    buckets.reduce((filled, bucket) => {
+      return bucket != null ? filled + 1 : filled;
+    }, 0) / buckets.length;
+  }
+
+  function expandBuckets(buckets) {
+    return buckets.concat(new Array(buckets.length).fill(null));
+  }
+
   function hash(key) {
     let hashCode = 0;
 
+    const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % buckets.length;
     }
+    console.log(`hash code: ${hashCode}`);
 
     return hashCode;
   }
+
+  function set(key, value) {
+    let loadFactor = getLoadFactor(buckets);
+    if (loadFactor >= loadThreshold) {
+      buckets = expandBuckets(buckets);
+    }
+    let bucketIndex = hash(key);
+
+    if (buckets[bucketIndex] === null) {
+      buckets[bucketIndex] = value;
+    }
+  }
+
+  function getBuckets() {
+    return buckets;
+  }
+
+  return { set, getBuckets };
 }
+
+export default HashMap;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles/style.css";
 import "./styles/fonts.css";
 import webpack_img from "./assets/images/webpack_img.webp";
-
+import HashMap from "./hash-map";
 const content = document.querySelector("#content");
 // content.textContent = "Hello Webpack";
 console.log("hello?");
@@ -14,6 +14,12 @@ const myIcon = new Image();
 myIcon.src = webpack_img;
 content.appendChild(myIcon);
 
+const table = HashMap();
+
+console.log(table.getBuckets());
+table.set("Carlos", "Testing value");
+
+console.log(table.getBuckets());
 // function component() {
 //   const element = document.createElement("div");
 
